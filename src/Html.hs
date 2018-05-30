943bf1b323d1fcacfc28f82de55292519612de56
Daniel Bramucci <dbramuccicode@gmail.com>
2018-05-30 16:32:24 -0500

    Wrote a simple preliminary compilation function.

--- src/Html.hs
+++ src/Html.hs
@@ -1,7 +1,27 @@
 module Html where
 
+import qualified Data.Text as T
+
 import Text.Blaze
 import Text.Blaze.Html5
+import Text.Blaze.Renderer.Utf8 (renderMarkup)
 
-AlmuToHtml :: Term -> Html
-AlmuToHtml = undefined
+import qualified Data.ByteString.Lazy as B
+import Control.Monad (mapM_)
+
+import Term
+
+almuToHtml' :: Term -> Html
+almuToHtml' (Text t) = toHtml t
+almuToHtml' (Symbol (SymbolName t)) = b $ toHtml t
+almuToHtml' (Env Title ts) = h1 $ mapM_ almuToHtml' ts
+almuToHtml' (Env Author ts) = h2 $ mapM_ almuToHtml' ts
+almuToHtml' (Env Date ts) = h2 $ mapM_ almuToHtml' ts
+almuToHtml' (Env Section ts) = h2 $ mapM_ almuToHtml' ts
+almuToHtml' (Env Subsection ts) = h3 $ mapM_ almuToHtml' ts
+almuToHtml' (Env Code ts) = code $ mapM_ almuToHtml' ts
+almuToHtml' (Env Displaymath ts) = error "Sorry no math support yet"
+almuToHtml' (Env Math ts) = error "Sorry no math support yet"
+
+almuToHtml :: [Term] -> B.ByteString
+almuToHtml ts = renderMarkup $ docTypeHtml . body $ mapM_ almuToHtml' ts
