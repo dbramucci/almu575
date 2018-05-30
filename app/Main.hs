40006878c87b2489770239fabb9ffa407a8afa50
Daniel Bramucci <dbramuccicode@gmail.com>
2018-05-30 17:04:25 -0500

    Finished a rough proof of concept for the compiler.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy as B
+import Text.Trifecta (Result(..))
+import System.FilePath.Windows
+import Parser (parseFull)
+import Html (almuToHtml)
+
 main :: IO ()
-main = undefined
+main = do
+    [file] <- getArgs :: IO [String]
+    let resultFile = addExtension (dropExtension file) ".html"
+    text <- readFile file
+    case almuToHtml <$> parseFull text of
+        Success htm -> B.writeFile resultFile htm
+        Failure err -> print err
+
