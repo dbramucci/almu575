49287ce40d4fa3fc816c2b61c5cff172149978c5
Daniel Bramucci <dbramuccicode@gmail.com>
2018-05-31 11:43:50 -0500

    Added centering to the title, author and date.

--- src/Html.hs
+++ src/Html.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Html where
 
 import qualified Data.Text as T
 
 import Text.Blaze
-import Text.Blaze.Html5
+import Text.Blaze.Html5 as Html5
+import qualified Text.Blaze.Html5.Attributes as Att
 import Text.Blaze.Renderer.Utf8 (renderMarkup)
 
 import qualified Data.ByteString.Lazy as B
@@ -11,12 +14,15 @@ import Control.Monad (mapM_)
 
 import Term
 
-almuToHtml' :: Term -> Html
+center :: Html -> Html
+center = Html5.div ! Att.style "text-align:center;"
+
+almuToHtml' :: Term -> Html 
 almuToHtml' (Text t) = toHtml t
 almuToHtml' (Symbol (SymbolName t)) = b $ toHtml t
-almuToHtml' (Env Title ts) = h1 $ mapM_ almuToHtml' ts
-almuToHtml' (Env Author ts) = h2 $ mapM_ almuToHtml' ts
-almuToHtml' (Env Date ts) = h2 $ mapM_ almuToHtml' ts
+almuToHtml' (Env Title ts) = Html5.div . center . h1 $ mapM_ almuToHtml' ts
+almuToHtml' (Env Author ts) = center . h2 $ mapM_ almuToHtml' ts
+almuToHtml' (Env Date ts) = center . h2 $ mapM_ almuToHtml' ts
 almuToHtml' (Env Section ts) = h2 $ mapM_ almuToHtml' ts
 almuToHtml' (Env Subsection ts) = h3 $ mapM_ almuToHtml' ts
 almuToHtml' (Env Code ts) = code $ mapM_ almuToHtml' ts
@@ -24,4 +30,4 @@ almuToHtml' (Env Displaymath ts) = error "Sorry no math support yet"
 almuToHtml' (Env Math ts) = error "Sorry no math support yet"
 
 almuToHtml :: [Term] -> B.ByteString
-almuToHtml ts = renderMarkup $ docTypeHtml . body $ mapM_ almuToHtml' ts
+almuToHtml ts = renderMarkup $ docTypeHtml .  Html5.div . body $ mapM_ almuToHtml' ts
